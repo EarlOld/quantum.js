e54d1d0b98d1485d98ea5b2c6f5a389daa07d789
Сапожник Дмитро <dimonsuper010@gmail.com>
2025-05-08 21:50:01 +0300

    Bump version to 0.4.6 in package.json and update footer in theme.config.tsx

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -36,7 +36,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/EarlOld/quantum.js',
   footer: {
-    text: 'MIT 2024 © EarlOld - Quantum.js@0.4.5',
+    text: 'MIT 2024 © EarlOld - Quantum.js@0.4.6',
   },
 };
 
